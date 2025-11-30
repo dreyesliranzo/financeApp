80942d6c2b013ba9c4dedba1261b95dd448fe2e9
dreyesliranzo <dreyesliranzo23@gmail.com>
2025-11-29 21:48:53 -0500

    Tune DB pool recycle/timeout to mitigate dropped connections

--- finance_app/config.py
+++ finance_app/config.py
@@ -28,4 +28,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_pre_ping": True,
+        "pool_recycle": 280,
+        "pool_timeout": 30,
     }
